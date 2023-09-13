37e8e86ae20ee810d62b90a1a8cb32321985ed00
nutaro <nutaro@protonmail.com>
2023-09-13 10:47:56 -0300

    add health check

--- backend/index.js
+++ backend/index.js
@@ -11,30 +11,30 @@ const database = process.env.db_name
 const connectionString = `postgres://${user}:${password}@${host}:${db_port}/${database}`
 const client = new PG.Client(connectionString)
 
-let successfulConnection = false;
-
 http.createServer(async (req, res) => {
   console.log(`Request: ${req.url}`);
-
-  if (req.url === "/api") {
-
+  if (req.url === "/") {
     try {
+      await client.connect();
       res.setHeader("Content-Type", "application/json");
       res.writeHead(200);
       let result;
       result = (await client.query("SELECT * FROM users")).rows[0];
       const data = {
-        database: successfulConnection,
+        database: true,
         userAdmin: result?.role === "admin"
       }
-    res.end(JSON.stringify(data));
+      res.end(JSON.stringify(data));
     } catch (error) {
       console.error(error.stack);
     } finally {
       await client.end();
     }
-    
-  } else {
+  } else if (req.url === "/health") {
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end(JSON.stringify({health: "ok"}));
+  }else {
     res.writeHead(503);
     res.end("Internal Server Error");
   }
