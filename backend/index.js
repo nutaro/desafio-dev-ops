79b885063c77e51e30700ebebf63d0ce9b2c1441
nutaro <nutaro@protonmail.com>
2023-09-13 10:28:57 -0300

    solve multiple connections issue

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 import http from 'http';
-import PG from 'pg';
+import PG from 'pg'
 
 const port = Number(process.env.port);
 const user = process.env.db_user
 const host = process.env.db_url
 const db_port = process.env.db_port
 const password = process.env.db_password
+const database = process.env.db_name
 
-const client = new PG.Client(
-  `postgres://${user}:${password}@${host}:${db_port}`
-);
+const connectionString = `postgres://${user}:${password}@${host}:${db_port}/${database}`
+const client = new PG.Client(connectionString)
 
 let successfulConnection = false;
 
@@ -17,27 +17,23 @@ http.createServer(async (req, res) => {
   console.log(`Request: ${req.url}`);
 
   if (req.url === "/api") {
-    await client.connect()
-      .then(() => { successfulConnection = true })
-      .catch(err => console.error('Database not connected -', err.stack));
-
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-
-    let result;
 
     try {
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      let result;
       result = (await client.query("SELECT * FROM users")).rows[0];
-    } catch (error) {
-      console.error(error)
-    }
-
-    const data = {
-      database: successfulConnection,
-      userAdmin: result?.role === "admin"
-    }
-
+      const data = {
+        database: successfulConnection,
+        userAdmin: result?.role === "admin"
+      }
     res.end(JSON.stringify(data));
+    } catch (error) {
+      console.error(error.stack);
+    } finally {
+      await client.end();
+    }
+    
   } else {
     res.writeHead(503);
     res.end("Internal Server Error");
